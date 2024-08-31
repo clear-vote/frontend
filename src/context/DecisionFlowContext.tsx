38cbd3ad919011318f504550bf573da8ecf7fd95
Robert Luiten <156024622+RobertLuiten@users.noreply.github.com>
2024-08-30 19:25:25 -0700

    Added new Favicon and branding files!

--- src/context/DecisionFlowContext.tsx
+++ src/context/DecisionFlowContext.tsx
@@ -4,20 +4,21 @@ import React, { createContext, useContext, useState } from 'react';
 import { Election, Politigram } from '@/types/index';
 import { PinnedCandidates, HiddenCandidates } from '@/types/index';
 import { useMediaQuery } from '@mui/material';
+import Toolbar from '@/app/components/Toolbar';
 
 interface DecisionFlowContextProps {
   elections: Record<number, Election>;
   setElections: React.Dispatch<React.SetStateAction<Record<number, Election>>>;
-  
+
   selectedElection: number | null;
   setSelectedElection: React.Dispatch<React.SetStateAction<number | null>>;
-  
+
   selectedContest: number | null;
   setSelectedContest: React.Dispatch<React.SetStateAction<number | null>>;
 
   pinnedCandidates: PinnedCandidates;
   setPinnedCandidates: React.Dispatch<React.SetStateAction<PinnedCandidates>>;
-  
+
   hiddenCandidates: Record<number, Record<number, Set<number>>>;
   setHiddenCandidates: React.Dispatch<React.SetStateAction<Record<number, Record<number, Set<number>>>>>;
 
@@ -38,21 +39,24 @@ export const DecisionFlowProvider: React.FC<{ children: React.ReactNode }> = ({ 
   const [hiddenCandidates, setHiddenCandidates] = useState<HiddenCandidates>({});
   const [selectedPolitigram, setSelectedPolitigram] = useState<Politigram | null>(null);
   const [isDesktop, setIsDesktop] = useState<boolean>(useMediaQuery('(min-width:600px)'));
-
   return (
-    <DecisionFlowContext.Provider
-      value={{
-        elections, setElections,
-        selectedElection, setSelectedElection,
-        selectedContest, setSelectedContest,
-        pinnedCandidates, setPinnedCandidates,
-        hiddenCandidates, setHiddenCandidates,
-        selectedPolitigram, setSelectedPolitigram,
-        isDesktop, setIsDesktop
-      }}
-    >
-      {children}
-    </DecisionFlowContext.Provider>
+    <div style={{ paddingTop: '45px' }}>
+      <Toolbar />
+      <DecisionFlowContext.Provider
+        value={{
+          elections, setElections,
+          selectedElection, setSelectedElection,
+          selectedContest, setSelectedContest,
+          pinnedCandidates, setPinnedCandidates,
+          hiddenCandidates, setHiddenCandidates,
+          selectedPolitigram, setSelectedPolitigram,
+          isDesktop, setIsDesktop
+        }}
+      >
+        {children}
+      </DecisionFlowContext.Provider>
+    </div>
+
   );
 };
 
